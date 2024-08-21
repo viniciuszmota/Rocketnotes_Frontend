70dbe11da4a2434eda9b476af2b2cde012098b01
vinicius <vinicius.quimica@gmail.com>
2024-08-21 14:18:01 -0300

    signin rocketnotes

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { ThemeProvider } from 'styled-components'
-import GlobalStyles from './styles/global'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { ThemeProvider } from "styled-components"
 
-import theme from './styles/theme'
+import GlobalStyles from "./styles/global"
 
-import { Routes } from './routes'
+import theme from "./styles/theme"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { AuthProvider } from "./hooks/auth"
+
+import { Routes } from "./routes"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Routes />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,12 +1,24 @@
-import { FiMail, FiLock } from 'react-icons/fi'
-import { Link } from 'react-router-dom'
+import { useState } from "react"
+import { FiMail, FiLock } from "react-icons/fi"
+import { Link } from "react-router-dom"
 
-import { Input } from '../../components/Input'
-import { Button } from '../../components/Button'
+import { Input } from "../../components/Input"
+import { Button } from "../../components/Button"
 
-import { Container, Form, Background } from './styles'
+import { useAuth } from "../../hooks/auth"
+
+import { Container, Form, Background } from "./styles"
 
 export function SignIn() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const { signIn } = useAuth()
+
+  function handleSignIn() {
+    signIn({ email, password })
+  }
+
   return (
     <Container>
       <Form>
@@ -19,15 +31,17 @@ export function SignIn() {
           placeholder="E-mail"
           type="text"
           icon={FiMail}
+          onChange={(e) => setEmail(e.target.value)}
         />
 
         <Input
           placeholder="Senha"
           type="password"
           icon={FiLock}
+          onChange={(e) => setPassword(e.target.value)}
         />
 
-        <Button title="Entrar" />
+        <Button title="Entrar" onClick={handleSignIn} />
 
         <Link to="/register">Criar conta</Link>
       </Form>
